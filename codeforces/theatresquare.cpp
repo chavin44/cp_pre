138f1592482c7136e1e402dc77e5ae205c948372
Kevin Bheemarasetti <142635390+woowsel@users.noreply.github.com>
2024-03-21 20:38:45 -0500

    Create theatresquare.cpp

---
+++ codeforces/theatresquare.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a,b,c;
+    cin >> a >> b >>c;
+    long long value=ceil((double)b/c)*ceil((double)a/c);
+    cout<<value << endl;
+ 
+ 
+    return 0;
+}
