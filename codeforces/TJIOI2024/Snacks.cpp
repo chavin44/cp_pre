a55bbcd6489120df2cb88f4431ba6d304b6e07c8
Kevin Bheemarasetti <142635390+woowsel@users.noreply.github.com>
2024-05-12 10:10:55 -0500

    Create Snacks.cpp

---
+++ codeforces/TJIOI2024/Snacks.cpp
@@ -0,0 +1,12 @@
+//https://codeforces.com/group/yVAVUcL9au/contest/523552/problem/G
+#include <bits/stdc++.h>
+#include <string>
+ 
+using namespace std;
+ 
+int main() {
+    int a,b,c;
+    cin >> a >> b >>c;
+    cout << min(a,b) << endl;
+    return 0;
+}
